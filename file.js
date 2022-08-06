a7d03d1b20cfc1233f733528d32a90badcf95018
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-08-06 15:21:06 +0300

    Some Files

--- file.js
+++ file.js
@@ -1,9 +1,69 @@
 const fs = require("fs");
-fs.readFile("./blog/alaa1.txt",(err,data)=>{
-    if(err)
+// fs.readFile("./blog/alaa1.txt",(err,data)=>{
+//     if(err)
+//     {
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
+// console.log("Last Line");
+
+// fs.writeFile('./blog/alaa.txt','Hello World',()=>
+// {
+//     console.log("File was written");
+// });
+
+// fs.writeFile('./blog/alaa.txt','Hello again',()=>
+// {
+//     console.log("File was written");
+// });
+
+if(!fs.existsSync("./assets"))
+{
+    fs.mkdir('./assets',(err)=>{
+        if(err)
+        {
+            console.log(err);
+        }
+        console.log("Folder Created");
+    });
+}
+else
+{
+    fs.rmdir("./assets",(err) =>{
+        if(err)
+        {
+            console.log(err);
+        }
+        console.log("File Deleted");
+    });
+}
+
+if(fs.existsSync("./blog/deletme.txt"))
+{
+    fs.unlink("./blog/deletme.txt",(err)=>
     {
-        console.log(err);
-    }
-    console.log(data.toString());
-});
-console.log("Last Line");
+        if(err)
+        {
+            console.log(err);
+        }
+        console.log("File deleted");
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
