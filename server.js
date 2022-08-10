40860f29c33e3409498525fbce8481c5d5b104e9
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-08-10 17:20:28 +0300

    Commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const server =  require("http")
 const curServer = server.createServer((req,res) =>{
-    console.log("Hello");
+    // console.log("Hello");
+    res.write("Hello")
 });
 curServer.listen("3000","localhost", ()=>{
     console.log("Listening for request");
