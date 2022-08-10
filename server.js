a1e9418047586cd18a17c86facc85067d900f5b5
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-08-10 18:09:08 +0300

    Commit

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const curServer = server.createServer((req,res) =>{
     // res.write("Hello")
     // console.log(req.method);
     res.setHeader("Content-Type","text/html")
-    res.write("<head><link rel=stylesheet href='#'></head>");
-    res.write("<p>Hello everyone</p>");
-    res.write("<p>Hello everyone</p>");
+    // res.write("<head><link rel=stylesheet href='#'></head>");
+    // res.write("<p>Hello everyone</p>");
+    // res.write("<p>Hello everyone</p>");
     res.end();
 });
 curServer.listen("3000","localhost", ()=>{
